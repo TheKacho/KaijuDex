264741888bf4838dd4dab578d469fd5cd688a5dc
TheKacho <abyssknightzero@gmail.com>
2022-07-14 21:12:00 -0400

    added list of monsters and movies to be seeded to a DB, may possibly hae to delete the previous DB file to proceed.

--- Context.cs
+++ Context.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 
 public class Context : DbContext
@@ -9,6 +10,52 @@ public class Context : DbContext
         options.UseSqlite(@"Data Source=KaijuDex.db");
     }
    
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+    //     List<Monster> source = new List<Monster>();
+
+    //     using(StreamReader p = new StreamReader("Monster.json")
+    //     {
+    //         string json = p.ReadToEnd();
+    //         source = JsonSerializer.Deserialize<List<Monster>>(json);
+    //     }
+    //     foreach (Monster p in source)
+    //     {
+    //         modelBuilder.Entity<Monster>().HasData(
+    //             new Monster
+    //             {
+    //                 Title = p.Title,
+    //                 Creator = p.Creator,
+    //                 BirthYear = p.BirthYear,
+    //                 SpecialAttack = p.SpecialAttack,
+    //             }
+    //         );
+    //         base.OnModelCreating(modelBuilder);
+    //     }
+    // }
+     protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Monster>().HasData(
+            new Monster { ID = 1, Title = "Godzilla", Creator ="Tomoyuki Tanaka", BirthYear = 1954, SpecialAttack = "Atomic Breath"},
+            new Monster { ID = 2, Title = "Mothra", Creator ="Shinichiro Nakamura", BirthYear = 1961, SpecialAttack = "SuperSonic Flight"},
+            new Monster { ID = 3, Title = "King Ghidorah", Creator ="Tomoyuki Tanaka", BirthYear = 1964, SpecialAttack = "Triple Thunder Blast"},
+            new Monster { ID = 4, Title = "Gamera", Creator ="Yonejiro Saito", BirthYear = 1965, SpecialAttack = "Firebreath"},
+            new Monster { ID = 5, Title = "Mecha Godzilla", Creator ="Unknown", BirthYear = 1974, SpecialAttack = "Space Beam"},
+            new Monster { ID = 6, Title = "King Kong", Creator ="Edgar Wallace", BirthYear = 1933, SpecialAttack = "Giant Punch"}
+        );
+
+        modelBuilder.Entity<Movie>().HasData(
+            new Movie{Id = 1, Title = "Godzilla", Director = "Ishiro Honda", YearReleased = 1954},
+            new Movie{Id = 2, Title = "Mothra", Director = "Ishiro Honda", YearReleased = 1961},
+            new Movie{Id = 3, Title = "Ghidorah, the Three Headed Monster", Director = "Ishiro Honda", YearReleased = 1964},
+            new Movie{Id = 4, Title = "Gamera, the Giant Monster", Director = "Noriaki Yuasa", YearReleased = 1965},
+            new Movie{Id = 5, Title = "Godzilla vs. Mechagodzilla", Director = "Jun Fukuda", YearReleased = 1974},
+            new Movie{Id = 6, Title = "King Kong", Director = "Merian C. Cooper", YearReleased = 1933}
+        );
+        
+        base.OnModelCreating(modelBuilder);
+    }
+    
    public DbSet<Monster>? Monsters {get;set;}
    public DbSet<Movie>? Movies {get; set;}
 }

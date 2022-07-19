a6e680d6bcf84f9a3c0b0757aa88e9298a1ab0d8
TheKacho <abyssknightzero@gmail.com>
2022-07-19 18:15:56 -0400

    Added more monsters to the list, added new migrations

---
+++ Migrations/20220719220237_NewConfig5.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KaijuDex.Migrations
+{
+    public partial class NewConfig5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

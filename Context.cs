9a49e94da9d6fdb6be2a0c6b1207973800f18b40
TheKacho <abyssknightzero@gmail.com>
2022-07-21 14:09:43 -0400

    modified the css and html index for the main page

--- Context.cs
+++ Context.cs
@@ -1,9 +1,10 @@
 using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
+using KaijuDex.Models;
 
 public class Context : DbContext
 {
-    private List<Monster> source;
+    
 
     public Context() : base(){}
     protected override void OnConfiguring(DbContextOptionsBuilder options)
@@ -19,33 +20,33 @@ public class Context : DbContext
 
     //     using(StreamReader p = new StreamReader("Monster.json")
     //     {
-    //         string json = p.ReadToEnd();
-    //         source = JsonSerializer.Deserialize<List<Monster>>(json);
+    //         monsters, json = p.ReadToEnd();
+    //         monsters = JsonSerializer.Deserialize<List<Monster>>(json);
     //     }
-    //     foreach (Monster p in source)
-    //     {
-    //         List<Monster> destination = source.Select(d =>new Monster())(
-    //             new Monster
-    //             {
-    //                 ID = d.ID,
-    //                 Title = d.Title,
-    //                 Creator = d.Creator,
-    //                 BirthYear = d.BirthYear,
-    //                 SpecialAttack = d.SpecialAttack,
-    //             }
-    //         );
-    //     }
+        // foreach (Monster p in source)
+        // {
+            
+        //         new Monster
+        //         {
+        //             ID = p.ID,
+        //             Title = p.Title,
+        //             Creator = p.Creator,
+        //             BirthYear = p.BirthYear,
+        //             SpecialAttack = p.SpecialAttack,
+        //         }
+            
+        // }
     
-    //         modelBuilder.Entity<Monster>().HasData(
-    //         new Monster { ID = 7, Title = "Rodan", Creator ="Ishiro Honda", BirthYear = 1956, SpecialAttack = "Sonic Wind Breath"},
-    //         new Monster { ID = 8, Title = "Anguirus", Creator ="Motoyoshi Oda", BirthYear = 1955, SpecialAttack = "Rolling Thunder Water"},
-    //         new Monster { ID = 9, Title = "Biollante", Creator ="Koichi Kawakita", BirthYear = 1989, SpecialAttack = "Piercing Rose Vines"},
-    //         new Monster { ID = 10, Title = "Destoroyah", Creator ="Takao Okawara", BirthYear = 1995, SpecialAttack = "Micro Oxygen Beam"},
-    //         new Monster { ID = 11, Title = "Space Godzilla", Creator ="Unknown", BirthYear = 1994, SpecialAttack = "Corona Beam"},
-    //         new Monster { ID = 12, Title = "Jet Jaguar", Creator ="Masaaki Sano", BirthYear = 1975, SpecialAttack = "Sword of Light"}
-    //     );
-    //         base.OnModelCreating(modelBuilder);
-    //     }
+        //     modelBuilder.Entity<Monster>().HasData(
+        //     new Monster { ID = 7, Title = "Rodan", Creator ="Ishiro Honda", BirthYear = 1956, SpecialAttack = "Sonic Wind Breath"},
+        //     new Monster { ID = 8, Title = "Anguirus", Creator ="Motoyoshi Oda", BirthYear = 1955, SpecialAttack = "Rolling Thunder Water"},
+        //     new Monster { ID = 9, Title = "Biollante", Creator ="Koichi Kawakita", BirthYear = 1989, SpecialAttack = "Piercing Rose Vines"},
+        //     new Monster { ID = 10, Title = "Destoroyah", Creator ="Takao Okawara", BirthYear = 1995, SpecialAttack = "Micro Oxygen Beam"},
+        //     new Monster { ID = 11, Title = "Space Godzilla", Creator ="Unknown", BirthYear = 1994, SpecialAttack = "Corona Beam"},
+        //     new Monster { ID = 12, Title = "Jet Jaguar", Creator ="Masaaki Sano", BirthYear = 1975, SpecialAttack = "Sword of Light"}
+        // );
+        //     base.OnModelCreating(modelBuilder);
+        // }
     // }
      
     

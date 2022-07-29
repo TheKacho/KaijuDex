710dff7d67860ff2e97ce40c4ae67d2b194372ba
TheKacho <abyssknightzero@gmail.com>
2022-07-29 08:52:34 -0400

    Re-adjusted the background color to be brighter and added text outline to make the text easier to read

---
+++ Migrations/20220721225939_NewConfig7.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KaijuDex.Migrations
+{
+    public partial class NewConfig7 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
